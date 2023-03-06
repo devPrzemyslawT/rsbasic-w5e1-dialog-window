8272e04b8608aad3d0b823250471e13ffd13fb85
devPrzemyslawT <przemyslaw.tomaszewski.dev@gmail.com>
2023-03-06 18:07:18 +0100

    structure of app created, material ui installed, simply dialog window component used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import CustomizedDialogs from "./Components/Dialog/dialog";
+import { dialog1Title, dialog1Content } from "./Utils/data/data";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<CustomizedDialogs
+				title={dialog1Title}
+				content={dialog1Content}></CustomizedDialogs>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Utils/data/data.js
@@ -0,0 +1,4 @@
+export const dialog1Title = "Title for Dialog1 window";
+
+export const dialog1Content =
+	"Content for dialog1 window: Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.";
